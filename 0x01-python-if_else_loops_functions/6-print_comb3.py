c3bb078db4f0427512bafe3c90e64461bb6510c1
Hellen Trotter <m.hbodiba@gmail.com>
2023-11-28 10:07:01 -0800

    Print combo3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num01 in range(10):
+    for num02 in range(10):
+        if num02 > num01 and num01 != 8:
+            print("{}{}".format(num01,num02), end=", ")
+        elif num01 == 8 and num02 == 9:
+            print("{}{}".format(num01, num02))
+            break
