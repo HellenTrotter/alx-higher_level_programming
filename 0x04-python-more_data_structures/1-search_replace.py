d17558604ba8bd7a8d62f9efff9c01039a8b5cb4
Hellen Trotter <m.hbodiba@gmail.com>
2023-12-06 03:58:52 -0800

    Search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list_copy = []
+    for i in my_list:
+        if i  == search:
+            list_copy.append(replace)
+        else:
+            list_copy.append(i)
+    return list_copy
