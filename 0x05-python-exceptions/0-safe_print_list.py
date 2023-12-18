917183d5b87bc42009232cfd0114d141f1db570f
Hellen Trotter <m.hbodiba@gmail.com>
2023-12-18 05:53:42 -0800

    Print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    printed = 0
+    for n in range(0, x):
+
+        try:
+            print("{}".format(my_list[n]), end="")
+            printed += 1
+
+        except:
+            continue
+
+    print()
+    return printed
