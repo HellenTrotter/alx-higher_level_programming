25b8a1b4c0c8b2ec0404a2fc39d22aa856c7ad7d
Hellen Trotter <m.hbodiba@gmail.com>
2023-12-18 05:59:48 -0800

    Print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[n]), end="")
             printed += 1
 
-        except:
+        except IndexError:
             continue
 
     print()
