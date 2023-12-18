955fb0f8147a45a56745bc836ef5bc939d563bed
Hellen Trotter <m.hbodiba@gmail.com>
2023-12-18 06:45:05 -0800

    Print list integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    p_integers = 0
+    try:
+        for i in range(x):
+            value = my_list[i]
+            if type(value) == int:
+                print("{:d}".format(value), end="")
+                p_integers += 1
+
+
+    except(TypeError, ValueError):
+        pass
+
+    print()
+    return p_integers
