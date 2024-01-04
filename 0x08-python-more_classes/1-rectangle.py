c3ce3b0aaea2f3c6c9e205f2aa60fcc8be24bb2d
Hellen Trotter <m.hbodiba@gmail.com>
2024-01-04 05:51:19 -0800

    1 Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ initialisation of default width and height """
 
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """ width """
-
-        return self.__width
+        self.width = width
 
     @property
     def height(self):
@@ -23,13 +17,11 @@ class Rectangle:
 
         return self.__height
 
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >=0")
-        self.__width = value
+    @property
+    def width(self):
+        """ width """
+
+        return self.__width
 
     @height.setter
     def height(self, value):
@@ -39,10 +31,18 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
     def area(self):
-        return self.__width * self.__height
+        return self.__height * self.__width
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.__height == 0 or self.__width == 0:
             return 0
-        return self.__width * 2 + self.__height * 2
+        return self.__height * 2 + self.__width * 2
