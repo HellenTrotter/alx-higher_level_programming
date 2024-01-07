223653535e1fa1f39cfae98db68a5b00b3257b0c
Hellen Trotter <m.hbodiba@gmail.com>
2024-01-07 08:14:51 -0800

    First commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a function that prints a square with the character #"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for index in range(size):
+        print("#" * size)
