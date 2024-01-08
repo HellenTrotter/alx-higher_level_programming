0f4b68a5c6bf68c32c78130994eeb1b9d84831f5
Hellen Trotter <m.hbodiba@gmail.com>
2024-01-08 06:04:10 -0800

    Inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """inherits_from method"""
 
+
 def inherits_from(obj, a_class):
     """ function that returns True if the object is an instance of
     a class that inherited (directly or indirectly) from the
